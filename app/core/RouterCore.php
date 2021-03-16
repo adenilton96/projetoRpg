4f8ade278cc3de8a7d0ef12a43efe7b0a8d2982a
adenilton  oliveira <adenilton96@live.com>
2021-03-15 21:24:28 -0300

    criando estrutura

--- app/core/RouterCore.php
+++ app/core/RouterCore.php
@@ -2,10 +2,21 @@
 
 namespace app\core;
 
+
 class RouterCore
 {
+    private $uri;
+    private $method;
+
     public function __construct()
     {
-        echo "core";
+        $this->initialize();
+    }
+
+    private function initialize()
+    {
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        $uri = $_SERVER['REQUEST_URI'];
+        dd($uri);
     }
 }
